a27d2176914a99b6ce8f3bcbc5bc80750e529749
Tai <anhtai3d2y@gmail.com>
2021-12-17 15:53:41 +0700

    connect to database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,15 @@
+import db from "../models/index";
 
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll()
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs')
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        })
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports = {
